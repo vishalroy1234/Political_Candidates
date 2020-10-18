4c42700ccd3140f854f755d85d7fea81b6f1d45e
Vishal Roy <vishalroyfiitjee1998@gmail.com>
2020-10-18 11:07:09 +0530

    Updates

---
+++ precache-manifest.bdbacbeba560f7dd1be6539b7af58fb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20b6bdb25dadc90adda23afab71dacd",
+    "url": "/Political_Candidates/index.html"
+  },
+  {
+    "revision": "847b4010d377dca75336",
+    "url": "/Political_Candidates/static/css/main.a9b948a5.chunk.css"
+  },
+  {
+    "revision": "bed6a0e6c1911d374068",
+    "url": "/Political_Candidates/static/js/2.5b804ff8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Political_Candidates/static/js/2.5b804ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847b4010d377dca75336",
+    "url": "/Political_Candidates/static/js/main.98487f7d.chunk.js"
+  },
+  {
+    "revision": "0d387cbaaf18c8b5d353",
+    "url": "/Political_Candidates/static/js/runtime-main.b0d1372c.js"
+  }
+]);
